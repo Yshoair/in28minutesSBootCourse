78901df7864f26dd09c94b4ff584e1615e31ae4a
Yusuf Alaa-Eldeen <yshoair@gmail.com>
2018-01-31 13:44:40 +0200

    Hello world Bean Service

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfullwebservices.controller;
 
+import com.in28minutes.rest.webservices.restfullwebservices.model.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,10 @@ public class HelloWorldController {
     public String helloWorld(){
         return "Hello World !";
     }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("HelloWorld !");
+    }
+
 }
