e91543a52b370764321b4b65aebfef9dc9e279f4
Yusuf Alaa-Eldeen <yshoair@gmail.com>
2018-01-31 14:53:36 +0200

    Hello world with Bean with path variables

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.in28minutes.rest.webservices.restfullwebservices.controller;
 
 import com.in28minutes.rest.webservices.restfullwebservices.model.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,12 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("HelloWorld !");
     }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(name);
+    }
+
+
 
 }
